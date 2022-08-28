bbdbdd3464371563ee43b08268bf2aa62c3b6757
supertan <943648187@qq.com>
2022-08-28 16:54:48 +0800

    docs(src): 添加注释

--- feGRASS/main.cpp
+++ feGRASS/main.cpp
@@ -303,16 +303,16 @@ int main(int argc, const char * argv[]) {
         if (similarity_tree[i]==0){
             DEBUG_PRINT("copy_off_tree_edge Loop %d/%ld \t if(similarity_tree[i]==0)\n",i,copy_off_tree_edge.size());
             num_additive_tree++;
-            /**** Iteration Log. Yuo delete the printf call. ****/
+            /**** Iteration Log. You delete the printf call. ****/
             if ((num_additive_tree%64)==0) {
                 printf("num_additive_tree : %d\n", num_additive_tree);
             }
             spanning_tree.push_back(copy_off_tree_edge[i]);
-            //run tarjhan algorithm to get the upper bound
+            //run tarjan algorithm to get the upper bound
             stack<int> process;//to show the process of dfs
             int mark[M+1];//to show whether a point has been gone through
             int find[M+1];//Joint search set
-            int position_node[M+1];//restore the child point in the dfs at the time
+            int position_node[M+1];//restore the child point in the dfs at the time (代表已经遍历到的位置，便于下一次恢复)
             for (int j=0; j<M+1; j++) {
                 mark[j]=0;
                 find[j]=j;
@@ -322,18 +322,22 @@ int main(int argc, const char * argv[]) {
             mark[largest_volume_point]=1;
 
             //stop the search when the vertexes of edge have been found
-            while (mark[int(copy_off_tree_edge[i][0])]==0||mark[int(copy_off_tree_edge[i][1])]==0) {
-                for (int k=position_node[process.top()]-1; k<N; k++) {
-                    if (LG((process.top())-1,k)!=0&&mark[k+1]!=1&&k!=(process.top())-1) {
-                        position_node[process.top()]=k+1;
-                        mark[k+1]=1;
-                        process.push(k+1);
-                        break;
+            int edge_point1 = int(copy_off_tree_edge[i][0]);
+            int edge_point2 = int(copy_off_tree_edge[i][1]);
+            while (mark[edge_point1]==0||mark[edge_point2]==0) { //如果边的两点都没有遍历到
+                for (int k=position_node[process.top()]-1; k<N; k++) { //第一次while。 position_node初始值全1，k初始值是0
+                    if (LG((process.top())-1,k) != 0 && \   //k 和 top之间有边
+                            mark[k+1] != 1 &&\              //k 点没有遍历过
+                            k != (process.top())-1 ) {      //k 和 top 不是同一个点
+                        position_node[process.top()]=k+1;   
+                        mark[k+1]=1;                        
+                        process.push(k+1);                  
+                        break;                              //mark 了一个点，就while check一下
                     }
                     else if (k==N-1){
                         int a=process.top();
                         process.pop();
-                        find[a]=process.top();
+                        find[a]=process.top();              //貌似想构造以largest_volume_point为根的树，但是找到 两点the vertexes of edge又中断了
                     }
                 }
             }
